51877825a37575997c424b2d63989d842561ef06
stanlys96 <stanlyskwok@gmail.com>
2024-07-30 22:15:33 +0700

    waldo

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,11 +1,15 @@
 export function transformGoogleDriveUrl(url) {
-  const regex = /\/file\/d\/([a-zA-Z0-9_-]+)\//;
-  const match = url.match(regex);
+  try {
+    const regex = /\/file\/d\/([a-zA-Z0-9_-]+)\//;
+    const match = url?.match(regex);
 
-  if (match && match[1]) {
-    const fileId = match[1];
-    return `https://drive.google.com/uc?export=view&id=${fileId}`;
+    if (match && match[1]) {
+      const fileId = match[1];
+      return `https://drive.google.com/uc?export=view&id=${fileId}`;
+    }
+
+    return url;
+  } catch (e) {
+    return url;
   }
-
-  return url;
 }
